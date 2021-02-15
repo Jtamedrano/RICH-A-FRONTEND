e6a89dec0882b17faf30153980566b9a03604bbd
Jesse Medrano <Jtamedrano@gmail.com>
2021-02-15 12:04:07 -0800

    SummaryAbout scaffold complete

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
+import SummaryAbout from '../components/SummaryAbout';
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <SummaryAbout />
     </>
   );
 };
