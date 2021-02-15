d72cf86ef9915224cfc8c0684196c55588b02ca3
Jesse Medrano <Jtamedrano@gmail.com>
2021-02-15 11:52:03 -0800

    Scaffold (#1)
    * Initial commit
    
    * Scaffold of Nav and Hero init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h6>Logo</h6>
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Issues</li>
+          <li>Donate</li>
+        </ul>
+      </nav>
+    </nav>
+  );
+};
+
+export default Navbar;
