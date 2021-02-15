b57ec25cca9de41a2294e9ccc18b69d08b13a5fe
Jesse Medrano <Jtamedrano@gmail.com>
2021-02-15 12:00:00 -0800

    hero scaffod complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,25 @@
 const Hero = () => {
-  return <></>;
+  return (
+    <div>
+      <div>
+        <h1>Corvallis is ready for action</h1>
+        <p>
+          Every student deserves the opportunity to strive for a higher purpose,
+          but until our school board has leadership in place to accomplish this
+          goal, we run the risk of taking that away from our children.
+        </p>
+        <p>Vote Richard Arnold for School District Board _____</p>
+        <p>Let Richard know you're in!</p>
+        <form>
+          <input placeholder="First Name" />
+          <input placeholder="Last Name" />
+          <input placeholder="Email Address" />
+          <button>Sign Up</button>
+          <button>Donate</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
