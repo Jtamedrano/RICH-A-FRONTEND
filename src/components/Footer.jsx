988d52128900974cce672075b6af9d0aad8679fc
Jesse Medrano <Jtamedrano@gmail.com>
2021-02-15 14:11:45 -0800

    link styles in footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
+import NavLinks from './NavLinks';
+
 const Footer = () => {
   return (
     <footer>
       <div>
         <ul>
-          <li>Home</li>
-          <li>About Richard</li>
-          <li>Issues</li>
+          {NavLinks.map((link) => (
+            <Link to={link.url}>
+              <ul>{link.label}</ul>
+            </Link>
+          ))}
         </ul>
       </div>
       <p>This page is paid for by the Richard Arnold Foundation</p>
