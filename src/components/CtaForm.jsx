5f6ff0f41efd5ff19422ba8325b2cf83a2351305
Jesse Medrano <Jtamedrano@gmail.com>
2021-02-15 16:44:28 -0800

    Hero Style

--- src/components/CtaForm.jsx
+++ src/components/CtaForm.jsx
@@ -1,12 +1,55 @@
-const CtaForm = () => {
+import { Button, Col, Form, Input, Row } from 'antd';
+
+const CtaForm = ({ rootClass }) => {
+  const [form] = Form.useForm();
+
   return (
-    <form>
-      <input placeholder="First Name" />
-      <input placeholder="Last Name" />
-      <input placeholder="Email Address" />
-      <button>Sign Up</button>
-      <button>Donate</button>
-    </form>
+    <Form layout="vertical" form={form} className={rootClass}>
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} wrap={true}>
+        <Col span="12">
+          <Form.Item label="First Name" name="firstname">
+            <Input
+            // className={`${rootClass}__FirstNameInput`}
+            // placeholder="First Name"
+            />
+          </Form.Item>
+        </Col>
+        <Col span="12">
+          <Form.Item label="Last Name" name="lastname">
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row>
+        <Col span="24">
+          <Form.Item label="Email" name="email">
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row justify="center" gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        <Col span="12" flex="auto">
+          <Button
+            style={{ width: '100%' }}
+            type="primary"
+            className={`${rootClass}__SignUpButton`}
+          >
+            Sign Up
+          </Button>
+          {/* <button className={`${rootClass}__SignUpButton`}>Sign Up</button> */}
+        </Col>
+        <Col span="12" flex="auto">
+          <Button
+            style={{ width: '100%' }}
+            type="primary"
+            className={`${rootClass}__DonateButton`}
+          >
+            Donate
+          </Button>
+          {/* <button className={`${rootClass}__DonateButton`}>Donate</button> */}
+        </Col>
+      </Row>
+    </Form>
   );
 };
 
--- src/views/Home/Hero.jsx
+++ src/views/Home/Hero.jsx
@@ -1,9 +1,22 @@
+import { Row, Col } from 'antd';
+import React from 'react';
 import CtaForm from '../../components/CtaForm';
 
 const Hero = () => {
   return (
-    <div>
-      <div>
+    <Row
+      className="hero"
+      justify="space-between"
+      align="middle"
+      gutter={[16, 16]}
+    >
+      <Col
+        span={12}
+        xs={{ order: 2, span: 24 }}
+        sm={{ order: 2, span: 24 }}
+        md={{ order: 1, span: 12 }}
+        className="heroText"
+      >
         <h1>Corvallis is ready for action</h1>
         <p>
           Every student deserves the opportunity to strive for a higher purpose,
@@ -12,11 +25,19 @@ const Hero = () => {
         </p>
         <p>Vote Richard Arnold for School District Board _____</p>
         <p>Let Richard know you're in!</p>
-        <CtaForm />
-      </div>
-      <div>Image Here</div>
-    </div>
+        <CtaForm rootClass="heroCta" />
+      </Col>
+      <Col
+        span={12}
+        className="heroImage"
+        xs={{ order: 1, span: 24 }}
+        sm={{ order: 1, span: 24 }}
+        md={{ order: 2, span: 12 }}
+      >
+        <img src="/images/sb-logo.png" alt="Corvallis School Board Logo" />
+      </Col>
+    </Row>
   );
 };
 
-export default Hero;
+export default React.memo(Hero);
